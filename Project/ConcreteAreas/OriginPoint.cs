7beb7daf392879821fc6b165d46f33fa48567b0c
Brielle Davis <b.davis@rgu.ac.uk>
2025-11-26 22:28:12 +0000

    Changing origin point desc for game narrative

--- Project/ConcreteAreas/OriginPoint.cs
+++ Project/ConcreteAreas/OriginPoint.cs
@@ -5,7 +5,8 @@ namespace Project;
 public class OriginPoint : AreaBase
 {
     public override string Name => "Origin Point";
-    public override string Description => "This is the starting point of your adventure, a place of beginnings and endless possibilities.";
+    public override string Description => "You awake in a clearing. The air is charged with something that tastes quite like magic." +
+        "This is the starting point of your adventure.";
     public override int X => 0;
     public override int Y => 0;
 
