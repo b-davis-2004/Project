ece56bc4475f5e92d59c5c7aad55b0622f13cf31
b-davis-2004 <b.davis@rgu.ac.uk>
2025-11-26 00:24:26 +0000

    Updating Mountain Pass Area description to include riddle hint

--- Project/ConcreteAreas/MountainPass.cs
+++ Project/ConcreteAreas/MountainPass.cs
@@ -4,7 +4,7 @@ namespace Project
     public class MountainPass : AreaBase
     {
         public override string Name => "Mountain Pass";
-        public override string Description => "This is the treacherous mountain pass, with steep cliffs and howling winds.";
+        public override string Description => "This is the treacherous mountain pass, with steep cliffs and howling winds. You can hear water running down hill in the distance.";
 
         public override int X => 2;
         public override int Y =>  0;
